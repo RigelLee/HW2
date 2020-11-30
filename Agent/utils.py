a267ecb026a6a2eaa2f29e215095d461c5fa75ae
rwy233 <rwy233@163.com>
2020-12-01 00:20:05 +0800

    Add files via upload
    unit_type分成5类

--- Agent/utils.py
+++ Agent/utils.py
@@ -20,8 +20,14 @@ _MINIMAP_UNIT_TYPE = features.MINIMAP_FEATURES.unit_type.index
 
 _SCREEN_PLAYER_ID = features.SCREEN_FEATURES.player_id.index
 _SCREEN_UNIT_TYPE = features.SCREEN_FEATURES.unit_type.index
-
-
+'''
+Marine = 48
+Zergling = 105
+Roach = 110
+Baneling = 9
+SCV = 45
+'''
+unitList = [48,105,110,9,45]
 def preprocess_minimap(minimap):
   layers = []
   assert minimap.shape[0] == len(features.MINIMAP_FEATURES)
@@ -31,7 +37,15 @@ def preprocess_minimap(minimap):
     elif features.MINIMAP_FEATURES[i].type == features.FeatureType.SCALAR:
       layers.append(minimap[i:i+1] / features.MINIMAP_FEATURES[i].scale)
     elif i == _MINIMAP_UNIT_TYPE:
-      layers.append(minimap[i:i+1] / features.MINIMAP_FEATURES[i].scale)
+      '''
+        unitList 是7个minigame中所有的unit种类，其他的不会在minigame中出现
+      '''
+      layer = np.zeros([ len(unitList) , minimap.shape[1], minimap.shape[2]], dtype=np.float32)
+      for j in range(unitList):
+        indy, indx = (minimap[i] == unitList[j]).nonzero()
+        layer[j, indy, indx] = 1
+      layers.append(layer)
+      #layers.append(minimap[i:i+1] / features.MINIMAP_FEATURES[i].scale)
     else:
       layer = np.zeros([features.MINIMAP_FEATURES[i].scale, minimap.shape[1], minimap.shape[2]], dtype=np.float32)
       for j in range(features.MINIMAP_FEATURES[i].scale):
@@ -45,8 +59,17 @@ def preprocess_screen(screen):
   layers = []
   assert screen.shape[0] == len(features.SCREEN_FEATURES)
   for i in range(len(features.SCREEN_FEATURES)):
-    if i == _SCREEN_PLAYER_ID or i == _SCREEN_UNIT_TYPE:
+    if i == _SCREEN_PLAYER_ID:
       layers.append(screen[i:i+1] / features.SCREEN_FEATURES[i].scale)
+    elif i == _SCREEN_UNIT_TYPE:
+      '''
+        unitList 是7个minigame中所有的unit种类，其他的不会在minigame中出现
+      '''
+      layer = np.zeros([ len(unitList) , screen.shape[1], screen.shape[2]], dtype=np.float32)
+      for j in range(unitList):
+        indy, indx = (screen[i] == unitList[j]).nonzero()
+        layer[j, indy, indx] = 1
+      layers.append(layer)      
     elif features.SCREEN_FEATURES[i].type == features.FeatureType.SCALAR:
       layers.append(screen[i:i+1] / features.SCREEN_FEATURES[i].scale)
     else:
@@ -66,7 +89,7 @@ def minimap_channel():
     elif features.MINIMAP_FEATURES[i].type == features.FeatureType.SCALAR:
       c += 1
     elif i == _MINIMAP_UNIT_TYPE:
-      c += 1
+      c += len(unitList)
     else:
       c += features.MINIMAP_FEATURES[i].scale
   return c
@@ -75,8 +98,10 @@ def minimap_channel():
 def screen_channel():
   c = 0
   for i in range(len(features.SCREEN_FEATURES)):
-    if i == _SCREEN_PLAYER_ID or i == _SCREEN_UNIT_TYPE:
+    if i == _SCREEN_PLAYER_ID:
       c += 1
+    elif i == _SCREEN_UNIT_TYPE:
+      c += len(unitList)
     elif features.SCREEN_FEATURES[i].type == features.FeatureType.SCALAR:
       c += 1
     else:
