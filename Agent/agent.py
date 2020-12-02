37bc3147a245cefac7b530eb61c4315e1319a2b3
yanmy-netizen <1800012797@pku.edu.cn>
2020-12-02 22:09:00 +0800

    Update agent.py

--- Agent/agent.py
+++ Agent/agent.py
@@ -24,7 +24,7 @@ class A3CAgent(object):
     self.ssize = ssize
     self.isize = len(actions.FUNCTIONS)
     self.beta = 1
-    self.eta = 100
+    self.eta = 0.001
 
 
   def setup(self, sess, summary_writer):
